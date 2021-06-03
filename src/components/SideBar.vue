84671efefbfdb52d639be437dc6193416644e161
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-06-03 11:47:27 +0630

    sidebar component

---
+++ src/components/SideBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="side">
+        <h3 class="re">Pilot List Result</h3>
+        <div class="btn-container">
+            <button class="bt">Pilot (1)</button>
+            <button class="bt">Pilot (2)</button>
+            <button class="bt">Pilot (3)</button>
+        </div>
+        
+    </div>
+ 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.side{
+    margin: 30px 10px;
+}
+.side .re{
+    color:#0097e6;
+}
+.btn-container{
+    margin-top: 40px;
+}
+.btn-container .bt{
+    display: block;
+    margin-top: 20px;
+    width: 300px;
+    text-align: center;
+    padding: 8px 10px;
+    border: 1px solid #000;
+    cursor: pointer;
+    font-size: 18px;
+    background-color: #f5f6fa;
+    transition: all 0.5s;
+    border-radius: 5px;
+}
+.btn-container .bt:hover{
+    transform: translateY(-5px);
+}
+</style>
--- src/components/StudenMark.vue
+++ src/components/StudenMark.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="table-container">
-            <h3>Pilot<span>(1)</span> Student Mark </h3>
-          <table border="3">
+        <div class="layout">
+            <div class="sidebar">
+               <SideBar></SideBar>
+            </div>
+           
+             <div class="result">
+                 <h3>Pilot<span>(1)</span> Student Mark </h3>
+                 <table border="3">
             <tr>
                 <th>Roll No</th>
                 <th>Name</th>
@@ -92,21 +98,44 @@
             </tr>
         
          
-    </table>
+                 </table>
+            </div>
+        </div>
+            
+           
     </div>
 </template>
 
 <script>
+import SideBar from './SideBar'
 export default {
+  components: { SideBar },
 
 }
 </script>
 
 <style>
+.layout{
+    display: flex;
+    justify-content: space-between;
+}
 .table-container{
-    width: 80%;
-    margin: 1px auto;
+    width: 96%;
     text-align: center;
+}
+.sidebar{
+    background-color: #f5f6fa;
+     width: 25%;
+     margin: 10px 0px 0px 5px;
+     border-radius: 5px;
+     transition: all 0.5s;
+
+}
+.sidebar:hover{
+    box-shadow: 0px 0px 15px rgba(0,0,0,0.5);
+}
+.result{
+    width: 73%;
 }
 .table-container h3{
     margin-bottom: 15px;
